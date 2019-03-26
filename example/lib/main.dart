5a0a2a6c1c7c08ae2a851e1abf1c36bc3a045463
devanshramen <devansh.ramen@telecom.mu>
2019-03-26 16:24:16 +0400

    added example

---
+++ example/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flurry/flurry.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    await Flurry.initialize(
+        androidKey: "QX7WZTNJ8468J6SR8DR3",
+        iosKey: "XCCWQH4MCD45JHSM4BYN",
+        enableLog: true);
+    await Flurry.setUserId("userId");
+    await Flurry.logEvent("eventName");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('A Flurry plugin example app'),
+        ),
+        body: Center(
+          child: Text('Should log event. Please close the app and wait a few seconds for events to be sent.'),
+        ),
+      ),
+    );
+  }
+}
